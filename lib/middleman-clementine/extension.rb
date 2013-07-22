0cd6001edbe4732b4e6628ee5cd552dd04101e46
Simon Rice <im@simonrice.com>
2013-07-22 03:05:42 +0100

    Moved to new style, added (non working) options

--- lib/middleman-clementine/extension.rb
+++ lib/middleman-clementine/extension.rb
@@ -1,22 +1,24 @@
+require "clementine"
 require "tilt"
 
 # Clementine extension
-module Middleman::Clementine
-
-  # Setup extension
-  class << self
-
-    # Once registered
-    def registered(app)
-      # Location of javascripts external to source directory.
-      # @return [Array]
-      #   set :js_assets_paths, ["#{root}/assets/javascripts/", "/path/2/external/js/repository/"]
-    
-      ::Sprockets.register_engine 'clj', ::Clementine::ClojureScriptTemplate
-      ::Sprockets.register_engine 'cljs', ::Clementine::ClojureScriptTemplate
-      ::Tilt.register 'clj', ::Clementine::ClojureScriptTemplate
-      ::Tilt.register 'cljs', ::Clementine::ClojureScriptTemplate
-    end
+module Clementine < Middleman::Extension
+  option :optimizations, :none, ""
+  option :pretty_print, false, ""
+  option :output_dir, nil, ""
+  
+  def initialize(app, options_hash={}, &block)
+    super
+  
+    ::Sprockets.register_engine 'clj', ::Clementine::ClojureScriptTemplate
+    ::Sprockets.register_engine 'cljs', ::Clementine::ClojureScriptTemplate
+    ::Tilt.register 'clj', ::Clementine::ClojureScriptTemplate
+    ::Tilt.register 'cljs', ::Clementine::ClojureScriptTemplate
+  end
+  
+  def after_configuration
+    Clementine[:output_dir] = :js_dir
+    Clementine[:optimizations] = :none
   end
 end
 
